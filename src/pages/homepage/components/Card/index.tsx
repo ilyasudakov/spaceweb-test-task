857480ec86acfe61b2acd745584e285df2e9ae7c
ilyasudakov <ilyasudakov.dev@gmail.com>
2022-09-08 17:53:07 +0300

    Add category icons & colors

--- src/pages/homepage/components/Card/index.tsx
+++ src/pages/homepage/components/Card/index.tsx
@@ -1,14 +1,32 @@
 import Button from "../../../../components/Form/Button/Button";
 import SelectInput from "../../../../components/Form/SelectInput";
 import { OptionsStateType } from "../Options/optionsReducer";
+import { ReactComponent as NVMeIcon } from "../../../../assets/tariff_nvme 1.svg";
+import { ReactComponent as HDDIcon } from "../../../../assets/tariff_hdd 1.svg";
+import { ReactComponent as TurboIcon } from "../../../../assets/tariff_turbo 1.svg";
 import styles from "./Card.module.scss";
 
 type VPS = OptionsStateType["data"]["vpsPlans"][number];
 type OS = OptionsStateType["data"]["selectOs"];
 type PANEL = OptionsStateType["data"]["selectPanel"];
 
+const CATEGORY_ICONS: { [s in string]: { icon: React.ReactNode; bg: string } } =
+  {
+    nvme: { icon: <NVMeIcon />, bg: "#E0F6FC" },
+    hdd: { icon: <HDDIcon />, bg: "#C4EBF8" },
+    turbo: { icon: <TurboIcon />, bg: "#ECFDF7" },
+  };
+
 export default function Card({
-  plan: { name, price_per_month, cpu_cores, ram, disk_type, volume_disk },
+  plan: {
+    name,
+    price_per_month,
+    cpu_cores,
+    ram,
+    disk_type,
+    volume_disk,
+    category,
+  },
   os,
   panel,
 }: {
@@ -17,8 +35,14 @@ export default function Card({
   panel: PANEL;
 }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.type}>{name}</div>
+    <div
+      className={styles.container}
+      style={{ backgroundColor: CATEGORY_ICONS[category].bg }}
+    >
+      <div className={styles.type}>
+        {CATEGORY_ICONS[category].icon}
+        {name}
+      </div>
       <div className={styles.price}>{`${price_per_month} ₽/мес.`}</div>
       <ul className={styles.hardware_list}>
         <Hardware label="CPU" text={`${cpu_cores}`} />
